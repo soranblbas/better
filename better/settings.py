cd581e56d8e62a264d3e01fd681d90f6113d796a
Tech <soran.hamad@epu.edu.iq>
2023-05-29 12:01:35 +0300

    new

--- better/settings.py
+++ better/settings.py
@@ -144,12 +144,12 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
-CACHES = {
-    'default': {
-        'BACKEND': 'django.core.cache.backends.db.DatabaseCache',
-        'LOCATION': 'my-cache-table',
-    }
-}
+# CACHES = {
+#     'default': {
+#         'BACKEND': 'django.core.cache.backends.db.DatabaseCache',
+#         'LOCATION': 'my-cache-table',
+#     }
+# }
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 LOGOUT_REDIRECT_URL = '/'
